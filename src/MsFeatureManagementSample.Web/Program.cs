e50c91d479d8fef4a86581ed9ffce795c66d8057
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-05-09 13:25:58 -0700

    add features

--- src/MsFeatureManagementSample.Web/Program.cs
+++ src/MsFeatureManagementSample.Web/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.FeatureManagement;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddFeatureManagement(builder.Configuration.GetSection("Features"));
+
 WebApplication app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 app.Run();
